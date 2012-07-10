6d0a8a61d41b77532858a26c1c2179b80df25e7f
Aaron Hicks (Landcare PN) <hicksa@landcareresearch.co.nz>
2012-07-10 16:27:56 +1200

    Install packages, and yum repo for Red Hatted distributions

--- manifests/service/install.pp
+++ manifests/service/install.pp
@@ -8,5 +8,28 @@
 class shibboleth::service::install(
 	
 ){
-	
+
+# Install packages
+	case $operatingsystem{
+		Ubuntu:{
+			package{'libshibsp4': ensure => present}
+			package{'libshibsp-doc': ensure => present}
+
+			package{'libapache2-mod-shib2':
+				ensure 	=> present,
+				require	=> Package['apache'],
+			}
+		}
+		CentOS:{
+			yumrepo { "shibboleth":
+			    descr => 'Shibboleth $releasever - $basearch',
+			    baseurl => 'http://download.opensuse.org/repositories/security:/shibboleth/${releasever}/${basearch}',
+			    enabled => 1,
+			    failovermethod => "priority",
+			    gpgcheck => 1,
+			    gpgkey => "http://download.opensuse.org/repositories/security:/shibboleth/${releasever}/repodata/repomd.xml.key",
+			}
+			package{'sibboleth': ensure => present}
+		}
+	}
 }
